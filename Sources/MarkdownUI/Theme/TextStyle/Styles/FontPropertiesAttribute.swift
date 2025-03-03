9d4629e3a4b90a1266ce57db9a8403b461a685b2
Tomasz Kaminski <tkaminski93@gmail.com>
2025-03-03 09:22:32 +0100

    removed print

--- Sources/MarkdownUI/Theme/TextStyle/Styles/FontPropertiesAttribute.swift
+++ Sources/MarkdownUI/Theme/TextStyle/Styles/FontPropertiesAttribute.swift
@@ -32,7 +32,6 @@ extension AttributedString {
       guard let fontProperties = run.fontProperties else {
         continue
       }
-      print(fontProperties.baselineOffset)
       output[run.range].baselineOffset = fontProperties.baselineOffset
       output[run.range].font = .withProperties(fontProperties)
       output[run.range].fontProperties = nil
